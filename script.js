b53c734cf4779f603fedf6eade67f00ca6cbd179
Anton Gerasimov <82588219+gerasimovtltsu@users.noreply.github.com>
2023-12-30 11:16:59 +0400

    Create script.js

---
+++ script.js
@@ -0,0 +1,11 @@
+function updateTime() {
+    var now = new Date();
+    var hours = now.getHours();
+    var minutes = now.getMinutes();
+    var seconds = now.getSeconds();
+    hours = hours < 10 ? '0' + hours : hours;
+    minutes = minutes < 10 ? '0' + minutes : minutes;
+    seconds = seconds < 10 ? '0' + seconds : seconds;
+    document.getElementById('time').innerHTML = hours + ':' + minutes + ':' + seconds;
+}
+setInterval(updateTime, 1000);
